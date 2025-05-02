16ebd78183b0a84cfe485555340b689e1bcdb29e
chuks <nwajeigoddowell@gmail.com>
2025-05-02 15:07:43 +0100

    custom auth

--- src/main/java/custom_manager/auth/filter/JwtAuthenticationFilter.java
+++ src/main/java/custom_manager/auth/filter/JwtAuthenticationFilter.java
@@ -27,7 +27,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         this.jwtAuthenticator = jwtAuthenticator;
     }
 
-
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
